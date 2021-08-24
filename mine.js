2c381504264b9eecc984bc587e83993180d0d18d
lokified <sheldon.obonyo@student.moringaschool.com>
2021-08-24 22:34:58 +0300

    add a function to checkbox

--- mine.js
+++ mine.js
@@ -1,10 +1,19 @@
 function hide(){
-    let x=document.getElementById("puts2");
+    let x=document.getElementById("puts");
     if(x.type === "password"){
         x.type="text";
 
     }
     else{
         x.type="password";
+    }
+    
+    let y=document.getElementById("puts2");
+    if(y.type === "password"){
+        y.type="text";
+
+    }
+    else{
+        y.type="password";
     }
 }
